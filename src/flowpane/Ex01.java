0a39a6df7ee88599b247e175b0d818c3785bcbe0
Choi.SY <sollly0829@gmail.com>
2021-08-30 15:12:31 +0900

    flowpane_ex01  flowpane 컨테이너 기본 예시

---
+++ src/flowpane/Ex01.java
@@ -0,0 +1,37 @@
+package flowpane;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class Ex01 extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+		FlowPane flowPane = new FlowPane();
+		
+//		Button btn01 = new Button("버튼01");
+//		Button btn02 = new Button("버튼02");
+//		Button btn03 = new Button("버튼03");
+//		
+//		flowPane.getChildren().add(btn01);
+//		flowPane.getChildren().add(btn02);
+//		flowPane.getChildren().add(btn03);
+		
+		for(int i=1;i<=3; i++) {
+			flowPane.getChildren().add(new Button("버튼0"+i));
+		}
+		Scene sc = new Scene(flowPane,200,100);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		launch(args);
+		
+	}
+	
+}
