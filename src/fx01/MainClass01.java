7986dfaf1d0ab2a9499a46966a95e586bdeec6cb
Choi.SY <sollly0829@gmail.com>
2021-08-30 14:10:15 +0900

    javafx 맛보기

--- src/fx01/MainClass01.java
+++ src/fx01/MainClass01.java
@@ -1,6 +1,9 @@
 package fx01;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;		// awt→swing→javafx
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainClass01 extends Application {
@@ -11,7 +14,18 @@ public class MainClass01 extends Application {
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		arg0.show();
+					//stage : 무대
+		
+		Label lb = new Label("Hello world");
+		lb.setFont(new Font(20));
+		
+		//Scene scene = new Scene(lb);
+		Scene scene = new Scene(lb, 200,100);	//가로 세로 사이즈
+							//  ↑ scene에는 무조건 하나만 들어갈 수 있다
+		
+		arg0.setScene(scene);
+		arg0.setTitle("연습중");	//타이틀 설정
+		arg0.show();			
 		
 	}
 	
