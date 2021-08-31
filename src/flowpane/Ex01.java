f18e32dcf295c76a342bc696dff05b7314d4341f
Choi.SY <sollly0829@gmail.com>
2021-08-31 10:04:22 +0900

    Flowpane 내용 추가

--- src/flowpane/Ex01.java
+++ src/flowpane/Ex01.java
@@ -1,6 +1,8 @@
 package flowpane;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
@@ -11,7 +13,8 @@ public class Ex01 extends Application {
 	@Override
 	public void start(Stage arg0) throws Exception {
 		
-		FlowPane flowPane = new FlowPane();
+		//FlowPane flowPane = new FlowPane();							//가로정렬
+		FlowPane flowPane = new FlowPane(Orientation.VERTICAL);	//세로정렬(옵션)
 		
 //		Button btn01 = new Button("버튼01");
 //		Button btn02 = new Button("버튼02");
@@ -21,12 +24,20 @@ public class Ex01 extends Application {
 //		flowPane.getChildren().add(btn02);
 //		flowPane.getChildren().add(btn03);
 		
-		for(int i=1;i<=3; i++) {
+		for(int i=1;i<=15; i++) {
 			flowPane.getChildren().add(new Button("버튼0"+i));
 		}
+		
+		//padding : 안쪽으로 여백을 준다.   (상,우,하,좌)
+		flowPane.setPadding(new Insets(10,10,10,10));
+		
+		flowPane.setVgap(80); 		//가로
+		flowPane.setHgap(10);		//세로
+		
 		Scene sc = new Scene(flowPane,200,100);
 		
 		arg0.setScene(sc);
+		arg0.setTitle("flowpane");
 		arg0.show();
 	}
 
@@ -35,5 +46,7 @@ public class Ex01 extends Application {
 		launch(args);
 		
 	}
+
+
 	
 }
